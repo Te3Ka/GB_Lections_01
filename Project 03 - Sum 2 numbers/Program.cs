a626ab85f39c6e89a53a86fb407c9722743d20fe
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-12 22:38:25 +0300

    Добавлен Рандом в программу 3

--- Project 03 - Sum 2 numbers/Program.cs
+++ Project 03 - Sum 2 numbers/Program.cs
@@ -1,5 +1,10 @@
-﻿int num1 = 3;
-int num2 = 8;
+﻿int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(1, 10);
 
 int sum = num1 + num2;
+Console.Write("Число 1 = ");
+Console.WriteLine(num1);
+Console.Write("Число 2 = ");
+Console.WriteLine(num2);
+Console.Write("Сумма двух чисел = ");
 Console.WriteLine(sum);
